faadbf46c6d7cbee6e5ab9893d1c7f2339190ffc
Diego Torres <dtorres@isimarkets.com>
2025-08-14 01:24:14 +0300

    minor fix

--- benchmark_usecase/benchmark_visualization.py
+++ benchmark_usecase/benchmark_visualization.py
@@ -1,12 +1,8 @@
-import json
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
-# from typing import List, Dict, Any, Union, Optional
-#importeme esta
 def create_risk_benchmark_dashboard(company_data):
     """
     Args:
